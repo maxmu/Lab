e33df7b4b55cd83eab1e891a4ff63308c5b3b82e
maxmu <maxmu.register@gmail.com>
2014-02-05 16:31:57 -0500

    simulation new commit

--- SNP_density/extract_SNP.py
+++ SNP_density/extract_SNP.py
@@ -1,6 +1,7 @@
 import Count
 import string
 import re
+import random
 
 class gene_class:
     def __init__(self, name, chromosome, intron, exon):
@@ -60,7 +61,57 @@ def construct_geneclass(gene):
     inputfile.close()
     return gene_class(str(gene), chromosome, introns_tuple_list, exons_tuple_list)
 
+def random_pick(size, array):
+    result = []
+    for i in range(size):
+        result.append(random.choice(array))
+    return result
 
-gene = construct_geneclass("uc001acx.1")
-print gene.intron
-        
+#This method could return a dictionary of SNP of specie
+def get_SNP_dict(specie):
+    inputfile = open("/home/xiaoyu/protein/SNP/" + specie + ".vcf", "r")
+    pattern = re.compile("chr(\S+)(\s+)(\S+)(\s+)\.")
+    SNP = {}
+    for line in inputfile:
+        line = string.strip(line)
+        match = pattern.match(line)
+        if match != None:
+            chromosome = match.group(1)
+            position = match.group(3)
+            if chromosome in SNP.keys():
+                SNP[chromosome].append(position)
+            else:
+                SNP[chromosome] = []
+                SNP[chromosome].append(position)
+    inputfile.close()
+    return SNP
+
+#gene should be a gene_class
+def get_intron_SNP(gene, SNP_dictionary):
+    intron_tuples = gene.intron
+    SNP_array_of_chromosome = SNP_dictionary[str(gene.chromosome)]
+    result = []
+    for tuple in intron_tuples:
+        for SNP in SNP_array_of_chromosome:
+            if int(SNP) > tuple[0] and int(SNP) < tuple[1]:
+                result.append(SNP)
+    return result
+
+not_LCR = Count.not_own_LCR_genes()
+array = random_pick(100, not_LCR)
+SNP_dict = get_SNP_dict("Homo")
+
+total_SNP = 0
+length = 0
+for i in array:
+    gene = construct_geneclass(i)
+    SNP_in_intron = get_intron_SNP(gene, SNP_dict)
+
+    for intron_tuple in gene.intron:
+        length += intron_tuple[1] - intron_tuple[0] + 1
+
+    print len(SNP_in_intron)
+    total_SNP += len(SNP_in_intron)
+
+density = float(float(total_SNP) / float(length))
+print density
